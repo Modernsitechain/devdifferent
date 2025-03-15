fd86fcc787d4091456c83652bcd85524ee810aec
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-03-16 00:29:47 +0700

    feat(layout): add navbar

--- src/app/shared/layouts/cms-layout/components/navbar/navbar.component.ts
+++ src/app/shared/layouts/cms-layout/components/navbar/navbar.component.ts
@@ -1,23 +1,10 @@
-import { Component, HostBinding, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  @Input()
-  @HostBinding('class.shadow')
-  public shadow = false;
-
-  constructor(private router: Router) {}
-
-  public goHome(): void {
-    // TODO: Change this navigate router to actual path
-    void this.router.navigate(['/']);
-  }
-}
+export class NavbarComponent {}
